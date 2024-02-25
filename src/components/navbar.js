5040fba628e1fccb962c4695e1f0e61d51f5233f
AnneMai <annepmaimain@gmail.com>
2024-02-25 11:02:03 -0800

    tryig pls 2

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/navbar.css';
+import Logo from "../images/SH.png";
 
 export default function Navigation(){
     return(
         <div>
             <Navbar>
                 <Container fluid>
-                    <Navbar.Brand />
+                    <Navbar.Brand href="/"><img src={Logo} alt="logo" width="30" height="30" /></Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse />
                         <Nav>
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/whatisshustainablle">Thingy</Nav.Link>
+                            <Nav.Link href="/profile">Profile</Nav.Link>
                         </Nav>
                 </Container>
             </Navbar>
---
+++ src/routes/badges.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navigation from '../components/navbar';
+
+export default function Badges(){
+    return(
+        <div>
+            <nav>
+                <Navigation />
+            </nav>
+            <h1>
+                BADGES
+            </h1>
+        </div>
+    )
+}
