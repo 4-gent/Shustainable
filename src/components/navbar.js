ed326bc031a6b4fddf128ed08e9d741cc6d0d86f
AnneMai <annepmaimain@gmail.com>
2024-02-25 17:48:39 -0800

    anne's merge...

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,6 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/navbar.css';
 import Logo from "../images/SH.png";
-<<<<<<< HEAD
 
 const fontStyle = {
     fontFamily: 'Monaco', 
@@ -11,9 +10,7 @@ const fontStyle = {
     color:'#36455A',
     fontWeight: 'bold'
 }
-=======
 // import signOn from '../routes/signOn';
->>>>>>> ac3d09f39d7fd7b5e19cfd89e852722b7f63c224
 
 export default function Navigation(){
     return(
@@ -23,21 +20,12 @@ export default function Navigation(){
                     <Navbar.Brand href="/" ><img src={Logo} alt="logo" width="40" height="40" /></Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse />
-<<<<<<< HEAD
                         <Nav >
                             <Nav.Link href="/" style={fontStyle}>HOME</Nav.Link>
                             <Nav.Link href="/whatisshustainablle" style={fontStyle}>Thingy</Nav.Link>
                             <Nav.Link href="/profile" style={fontStyle}>PROFILE</Nav.Link>
                             <Nav.Link href='/signOn' style={fontStyle}>LOGIN</Nav.Link>
-=======
-                        <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/whatisshustainablle">Thingy</Nav.Link>
-                            <Nav.Link href="/tips">Tips</Nav.Link>
-
-                            <Nav.Link href="/profile">Profile</Nav.Link>
-                            <Nav.Link href='/signOn'>Sign In</Nav.Link>
->>>>>>> ac3d09f39d7fd7b5e19cfd89e852722b7f63c224
+                            <Nav.Link href="/tips" style={fontStyle}>Tips</Nav.Link>
                         </Nav>
                 </Container>
             </Navbar>
