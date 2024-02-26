d130f5091a70b20887f4c3933b9886dbb74fc6fc
Marlon (Mj) Burog <spicyrice09@gmail.com>
2024-02-25 17:58:39 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./main";
-import IntroCarousel from "./routes/introCarousel";
-import Badges from "./routes/badges"
-import Tips_home from "./routes/Tips/Tips_home";
+import Shop from "./routes/shop";
+import Intro from "./routes/intro";
 
 export default function(){
   return(
     <Router>
       {/* Place routes here with exact file path */}
       <Routes>
-        <Route exact path="/" element={<Main />}/>
-        <Route exact path="/whatisshustainable" element={<IntroCarousel />}/>
-        <Route exact path="/profile" element={<Badges />}/>
-        {/* <Route exact path="/signOn" element={<signOn />}/> */}
-        <Route exact path="/tips" element={<Tips_home />}/>
+        <Route exact path="/" element={<Intro />}/>
+        {/* <Route exact path="/home" element={<Home />}/> */}
+        <Route exact path="/shop" element={<Shop />} />
       </Routes>
     </Router>
   )
