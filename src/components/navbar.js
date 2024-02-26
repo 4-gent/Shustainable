7ebacb187b1cd8d463d30a6f4c27d5b7b30058e6
AnneMai <annepmaimain@gmail.com>
2024-02-25 17:16:55 -0800

    have mercy

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,22 +3,27 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/navbar.css';
 import Logo from "../images/SH.png";
-import signOn from '../routes/signOn';
+
+const fontStyle = {
+    fontFamily: 'Monaco', 
+    fontSize: '20px', 
+    color:'#36455A',
+    fontWeight: 'bold'
+}
 
 export default function Navigation(){
     return(
         <div>
-            <Navbar>
+            <Navbar className='nav'>
                 <Container fluid>
-                    <Navbar.Brand href="/"><img src={Logo} alt="logo" width="30" height="30" /></Navbar.Brand>
+                    <Navbar.Brand href="/" ><img src={Logo} alt="logo" width="40" height="40" /></Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse />
-                        <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/whatisshustainablle">Thingy</Nav.Link>
-
-                            <Nav.Link href="/profile">Profile</Nav.Link>
-                            <Nav.Link href='/signOn'>Sign In</Nav.Link>
+                        <Nav >
+                            <Nav.Link href="/" style={fontStyle}>HOME</Nav.Link>
+                            <Nav.Link href="/whatisshustainablle" style={fontStyle}>Thingy</Nav.Link>
+                            <Nav.Link href="/profile" style={fontStyle}>PROFILE</Nav.Link>
+                            <Nav.Link href='/signOn' style={fontStyle}>LOGIN</Nav.Link>
                         </Nav>
                 </Container>
             </Navbar>
--- src/routes/badges.js
+++ src/routes/badges.js
@@ -1,15 +1,120 @@
 import React from "react"
 import Navigation from '../components/navbar';
+import '../App.css';
+import { styled } from '@mui/material/styles';
+import Badge from '../components/badgeThing';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Typography from '@mui/material/Typography';
+import Friend from "../components/friendThing";
+import { Button } from "react-bootstrap";
+
+import Pfp from '../images/face_icons/Pfp.jpeg';
+import Anne from '../images/face_icons/Anne.png';
+import Shreyass from '../images/face_icons/Shreyass.png';
+import MJ from '../images/face_icons/MJ.png';
+
+import Oil_Pic from '../images/badge_icons/Oil.png';
+import Tree_Pic from '../images/badge_icons/Tree.png';
+import Like_Pic from '../images/badge_icons/Like.png';
+import Sweet_Pic from '../images/badge_icons/Sweet.png';
+
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  });
 
 export default function Badges(){
     return(
-        <div>
-            <nav>
+        <div className="App">
+            <nav >
                 <Navigation />
             </nav>
-            <h1>
-                BADGES
-            </h1>
+            <div className={{display: 'flex'}}>
+                <div>
+                    <div style={{ height: '20vh', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop:'5vh'}}>
+                        <header>
+                            <img src={Pfp} alt="My Image" style={{width: '20vh', height: 'auto', borderRadius: '50%'}} />
+                        </header>
+                    </div>
+                    
+                    <div style={{ height: '15vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <Typography variant="body2" color="#36455A" fontSize="40px" fontFamily='Monospace' fontWeight='bold'>
+                            EJIRO IGUN
+                        </Typography>
+                    </div>
+
+                    <div style={{ marginBottom: '40px', marginLeft:'100px', marginRight:'100px', backgroundColor:'#C9EAE5', padding:'20px', borderRadius: '15px'}}>
+                        <Row style={{padding: '10px'}}>
+                            <Typography marginLeft='20px' variant="body2" color="#36455A" fontSize="30px" fontFamily='monospace' fontWeight='bold'>
+                                BADGES
+                            </Typography>
+                        </Row>
+                        <Row style={{padding: '10px'}}>
+                            <Col>
+                                <Badge Image={Tree_Pic} BadgeTitle={"PURVEYOR OF PLANTS"} BadgeDescription={"You just planted a tree. That’s pretty cool."}></Badge>
+                            </Col>
+                            <Col>
+                                <Badge Image={Oil_Pic} BadgeTitle={"ANTI OIL OLIGARCH"} BadgeDescription={"Use a hybrid/electric vehicle. Emission free zoomin’."}></Badge>
+                            </Col>
+                        </Row>
+                        <Row style={{padding: '10px'}}>
+                            <Col>
+                                <Badge Image={Like_Pic} BadgeTitle={"LIKIN' LOCAL"} BadgeDescription={"Like your first shustainable post. I dare you to save it too :03"}></Badge>
+                            </Col>
+                            <Col>
+                                <Badge Image={Sweet_Pic} BadgeTitle={"SWEET SUSTAINABILITY"} BadgeDescription={"Cash out on some sweet deals at your local farmer’s market. Discounted happiness."}></Badge>
+                            </Col>
+                        </Row>
+                        <Row style={{padding: '10px'}}>
+                            <Button style={{ backgroundColor: 'transparent', border: 'none'}}>
+                                <div style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: '20px' }}>
+                                    <a href="/friends">
+                                        <button style={{ backgroundColor:'#F9F7F1', fontWeight: 'bold', fontFamily: 'monospace', color: '#36455A', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer'}}>
+                                            MORE
+                                        </button>
+                                    </a>
+                                </div>
+                            </Button>
+                        </Row>
+                    </div>
+
+                    <div style={{ marginBottom: '40px', marginLeft:'100px', marginRight:'100px', backgroundColor:'#F7B492', padding:'20px', borderRadius: '15px'}}>
+                        <Row style={{padding: '10px'}}>
+                            <Typography marginLeft='20px' variant="body2" color="#36455A" fontSize="30px" fontFamily='monospace' fontWeight='bold'>
+                                FRIENDS
+                            </Typography>
+                        </Row>
+                        <Row style={{padding: '10px'}}>
+                            <Col>
+                                <Friend ProfilePic={Anne} FriendName={"ANNE MAI"} FavStore={"California Sourdough Eatery"}></Friend>
+                            </Col>
+                            <Col>
+                                <Friend ProfilePic={Shreyass} FriendName={"SHREYASS PREM SANKAR"} FavStore={"Campbell Farmer's Market"}></Friend>
+                            </Col>
+                            <Col>
+                                <Friend ProfilePic={MJ} FriendName={"MARLON BUROG"} FavStore={"MLK Library"}></Friend>
+                            </Col>
+                        </Row>
+                        <Row style={{padding: '10px'}}>
+                            <Button style={{ backgroundColor: 'transparent', border: 'none'}}>
+                                <div style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: '20px' }}>
+                                    <a href="/friends">
+                                        <button style={{ backgroundColor:'#F9F7F1', fontWeight: 'bold', fontFamily: 'monospace', color: '#36455A', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer'}}>
+                                            MORE
+                                        </button>
+                                    </a>
+                                </div>
+                            </Button>
+                        </Row>
+                    </div>
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
